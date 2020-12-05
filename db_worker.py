82d6b12b834c65348aec8f4ef306ac7a0308443b
Ardent-Mechanic <gikata.38@yandex.ru>
2020-12-05 13:03:55 +0300

    init change

--- db_worker.py
+++ db_worker.py
@@ -2,10 +2,9 @@ import sqlite3
 
 
 class DateBaseW:
-    def __init__(self, name='no_delete.db'):
-        self.name = name
-        self.db = sqlite3.connect(name)
-        self.cur = self.db.cursor()
+    def __int__(self, cur, database):
+        self.cur = cur
+        self.database = database
 
     def create_table(self, name1="article", name2="dop_info"):
 
@@ -16,7 +15,7 @@ class DateBaseW:
     date         DATE    NOT NULL,
     viwe         INTEGER
     genre        TEXT    NOT NULL);""")  # main info
-        self.db.commit()
+        self.database.commit()
 
         self.cur.execute(f"""CREATE TABLE {name2} (
     dop_id       INTEGER PRIMARY KEY ON CONFLICT ROLLBACK NOT NULL,
@@ -24,7 +23,7 @@ class DateBaseW:
     author_link TEXT,
     time         TIME    NOT NULL,
     rate         INTEGER);""")  # dop info
-        self.db.commit()
+        self.database.commit()
 
     def show_rows(self, rows_string):
 
@@ -34,15 +33,15 @@ class DateBaseW:
     def clear_table(self, com):
         if com == 1:
             self.cur.execute("DELETE FROM article;")
-            self.db.commit()
+            self.database.commit()
 
         elif com == 2:
             self.cur.execute("DELETE FROM dop_info;")
-            self.db.commit()
+            self.database.commit()
 
         else:
             self.cur.execute("DELETE FROM article;")
-            self.db.commit()
+            self.database.commit()
 
             self.cur.execute("DELETE FROM dop_info;")
-            self.db.commit()
+            self.database.commit()
