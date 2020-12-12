465e4b6504bd8dc3a425b1d2a8147ce90efb50f5
Ardent-Mechanic <gikata.38@yandex.ru>
2020-12-12 18:11:49 +0300

    minus replacement

--- script_for_parse.py
+++ script_for_parse.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import sqlite3
-
 import requests
 
 from bs4 import BeautifulSoup as bs
 
 from datetime import datetime, timedelta
 
-from time import sleep
-
 
 class Parser:
-    def __init__(self, w_db_class):
-        # def __init__(self, cur, database):
+    def __init__(self):
         self.header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
                                      " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
                        "accept": "*/*"}
@@ -22,9 +17,6 @@ class Parser:
                           'вчера': (datetime.today().date() - timedelta(days=1)).strftime("%d-%m-%Y"),
                           'января': 1, 'февраля': 2, 'марта': 3, 'апреля': 4, 'мая': 5, 'июня': 6,
                           'июля': 7, 'августа': 8, 'сентября': 9, 'октября': 10, 'ноября': 11, 'декабря': 12}
-        self.w_db_class = w_db_class
-        # self.cur = cur
-        # self.database = database
 
     def get_html(self, url):  # Получение html страницы
         text_link = requests.get(url, self.header)
@@ -57,25 +49,18 @@ class Parser:
 
         return self.date_dict[date] if date in ('сегодня', 'вчера') else create_date(date.split()), time
 
-    # def chek_article(self, new_article_name):
-    #
-    #     return self.cur.execute(f"""SELECT id FROM article WHERE article_name = '{new_article_name}'""")
-
     def content_filtering(self, box, gnr):  # Получение данных в более приятном виде
         all_data = []
         for art in box:
-
             article_name, article_link = art[1].get_text(), art[1]['href']  # Название статьи и ссылка на неё
-
-            # if mode == 1 and self.w_db_class.chek_article(
-            #         article_name).fetchone():  # Поиск статьи в базе данных и добавление
-            #     return [all_data, False]
 
             author_name, author_link, datatime = art[0][1].get_text().strip(), art[0][1]['href'], art[0][
                 3].get_text()  # Имя автора, ссылка на его профиль дата поста
 
-            rate, viwe = art[3][0].get_text().strip(), art[3][1].get_text().strip().replace(',',
-                                                                                            '.')  # Рейтинг и просмотры
+            # Рейтинг и просмотры
+            rate, viwe = art[3][0].get_text().strip(), art[3][1].get_text().strip().replace(',', '.')
+
+            rate = rate.replace('–', '-') if '–' in rate else rate
 
             viwe = float(viwe[:-1]) * 1000 if 'k' in viwe else float(viwe)
 
@@ -85,64 +70,27 @@ class Parser:
 
         return all_data
 
-    # def filling_database(self, all_data):
-    #
-    #     for data in all_data:
-    #         self.cur.execute(f"INSERT INTO article (article_name, author_name,"
-    #                          f" date, viwe, genre) VALUES (?, ?, ?, ?, ?)", data[:5])
-    #         self.db.commit()
-    #
-    #         self.cur.execute(f"INSERT INTO dop_info (article_link, author_link, time, rate) VALUES (?, ?, ?, ?)",
-    #         data[5:])
-    #         self.db.commit()
-
     def manual_parse(self, name, page):
-        # for page in self.get_page(n, f'https://habr.com/{name}/'):
         html = self.get_html(page)
 
         if html.status_code == 200:
             nxt = self.get_article(html.text)
             box = self.get_content(nxt)
             filter_box = self.content_filtering(box, name)
-            # print(filter_box)
             return filter_box
-            # self.w_db_class.filling_database(filter_box)
 
         else:
             print('Error')
             return
 
     def auto_parse(self, name, i):
-        # i = 1
-        # while True:
         html = self.get_html(f'https://habr.com/{name}/page{i}/')
 
         if html.status_code == 200:
             nxt = self.get_article(html.text)
-
-            # if len(nxt) == 0:
-            #     i = 0
-            # else:
             box = self.get_content(nxt)
-            filter_box= self.content_filtering(box, name)
+            filter_box = self.content_filtering(box, name)
             return filter_box
-            # self.filling_database(filter_box)
-            # if not com:
-            #     print(f'script sleep for {slp} second')
-            #     i = 0
-            #     sleep(slp)
         else:
             print('Error')
             return
-        # i += 1
-
-
-def main():
-    names = ['all', 'news', 'top']
-    boom = Parser('no_delete.db')
-    boom.get_page(1, f'https://habr.com/{names[0]}/')
-    boom.auto_parse(names[0], 1)
-
-
-if __name__ == "__main__":
-    main()
